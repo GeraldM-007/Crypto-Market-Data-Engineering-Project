3bda4ad78e4cf9f9926c591d29bb9c67d8420952
GeraldM-007 <geraldmuchuku237@gmail.com>
2026-06-17 23:46:48 +0300

    getting pairs from kraken

--- dag.py
+++ dag.py
@@ -1,1 +1,40 @@
+from airflow.sdk import task, dag
+from datetime import datetime, timedelta
+import json
+import requests
+import websocket
 
+@dag(
+    dag_id = 'crypto_prices_dag',
+    start_date = datetime(2026, 06, 18),
+    schedule = timedelta(minutes =5),
+    catchup = False
+)
+
+def crypto_market_prices():
+    @task()
+    def get_trading_pairs(limit=20):
+        
+        url = "https://api.kraken.com/0/public/AssetPairs"
+        
+        response =  requests.get(url)
+        data = response.json()
+        
+        pairs = []
+        
+        
+        for symbol, info in data['result'].items():
+         #Selecting only USD quote pairs
+         if info.get('quote') == 'ZUSD':
+             
+             pairs.append(info['wsname'])
+             
+             if len(pairs) == limit:
+                break
+        return pairs
+    
+    pairs = get_trading_pairs()
+    print(pairs)
+
+raw_data = crypto_market_prices()
+print(raw_data)
