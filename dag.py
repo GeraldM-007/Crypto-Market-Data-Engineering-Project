1153d50d709965c60c7ab9ee810209c342bcfdb4
GeraldM-007 <geraldmuchuku237@gmail.com>
2026-06-18 23:51:05 +0300

    pulling historical data from kraken

--- dag.py
+++ dag.py
@@ -27,14 +27,39 @@ def crypto_market_prices():
          #Selecting only USD quote pairs
          if info.get('quote') == 'ZUSD':
              
-             pairs.append(info['wsname'])
+             pairs.append(info['altname'])
              
              if len(pairs) == limit:
                 break
         return pairs
     
+    @task
+    def get_historical_prices(pairs):
+        
+        historical_data = []
+        
+        for pair in pairs:
+            
+            yesterday = datetime.today() - timedelta(days=1)
+            
+            since = yesterday.timestamp()
+            
+            url = f"https://api.kraken.com/0/public/OHLC?pair={pair}&interval=1440&since={since}"
+            
+            response = requests.get(url)
+            
+            response_data =  response.json()
+            
+            historical_data.append(response_data)
+            
+            return historical_data
+
+    
     pairs = get_trading_pairs()
     print(pairs)
+    
+    historical_prices = get_historical_prices(pairs)
+    print(historical_prices)
 
 raw_data = crypto_market_prices()
 print(raw_data)
