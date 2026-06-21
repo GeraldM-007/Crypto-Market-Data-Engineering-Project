26543272bbdd5299cabd9521bbe70ba66d18331b
GeraldM-007 <geraldmuchuku237@gmail.com>
2026-06-22 00:33:23 +0300

    updating with comments

--- realtime_data_dag.py
+++ realtime_data_dag.py
@@ -15,32 +15,63 @@ load_dotenv()
     catchup = False
 )
 
-def reatime_stocks_prices():
+def realtime_stocks_prices():
+    #task one, get trading pairs
     @task()
-    def extract_realtime_prices():
+    def get_trading_pairs(limit=10):
         
+        url = "https://api.kraken.com/0/public/AssetPairs"
+        
+        #query the API
+        response =  requests.get(url)
+        
+        #converting the response into a python dictionary
+        data = response.json()
+        
+        #store the pairs in a list
+        pairs = []
+        
+        #looping through the response to select only 10 ZUSD pairs
+        for symbol, info in data['result'].items():
+            
+            #Selecting only USD quote pairs
+            if info.get('quote') == 'ZUSD':
+                
+                #append the selected pairs into the list
+                pairs.append(info['altname'])
+                
+                #stop the lop once the lenght(10) has been reached
+                if len(pairs) == limit:
+                    break
+            
+        return pairs
+    
+    #task 2, extract prices for the trading pairs
+    @task()
+    def extract_realtime_prices(pairs):
+        
+        #historical data is only data from the previous day
+        #Calculating and converting the timestamp to current local time using current utcnow
         now = datetime.utcnow() + timedelta(hours = 3)
         date = now.strftime('%Y-%m-%d %H:%M:%S.%f')
         
-        pairs = ['BTC/USD', 'ETH/USD', 'DOGE/USD']
-        
+        #store the collected realtime data
         realtime_data = []
         
+        #loop through each trading pair collected
         for pair in pairs:
             
+            #kraken API url
             url = f'https://api.kraken.com/0/public/Ticker?pair={pair}'
             
+            #query the API
             response = requests.get(url)
             
+            #convert the json response into a python dictionary
             data = response.json()
             
-            result = list(data['result'].values())[0]
-            
-            realtime_data.append({
-                'pair' : pair,
-                'price' : float(result['c'][0]),
-                'timestamp' : date
-            })
+            realtime_data.append(data)
+    
         return realtime_data
     
     @task()
@@ -53,19 +84,29 @@ def reatime_stocks_prices():
         PORT = os.getenv('PORT')
         DB_NAME = os.getenv('DATABASE')
         
+        #define database connection
         engine = create_engine(f'postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}')
         
+        #connect to the database
         with engine.begine() as conn:
             
+            #loop through each item of the json output
             for item in realtime_data:
                 
+                #write the data into the database table
                 conn.execute(
                     text("""INSERT INTO realtime_crpto_prices(data) VALUES (:data)"""), {"data": json.dumps(item)}
                 )
     
-    realtime_data = extract_realtime_prices()
+    #call the trading pairs function and store output
+    pairs = get_trading_pairs()
     
+    #call the realtime prices function
+    realtime_data = extract_realtime_prices(pairs)
+    
+    #call the loading function
     load_data(realtime_data)
 
-dag = reatime_stocks_prices()
+#call the outer function
+dag = realtime_stocks_prices()
 
