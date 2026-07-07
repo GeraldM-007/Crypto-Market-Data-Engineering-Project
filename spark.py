b4b9675aa6abe6d961ea02b99edd377602d03d64
GeraldM-007 <geraldmuchuku237@gmail.com>
2026-07-08 01:35:11 +0300

    data transforming

--- spark.py
+++ spark.py
@@ -8,7 +8,7 @@ spark = SparkSession.builder.appName("crypto").config("spark.jars.packages", "or
 ohlc_stream_df = (
     spark.readStream
     .format("kafka") #defining the type of source spark is reading from
-    .option("kafka.bootstrap.servers", "3.144.131.176:9092") #tells spark how to connect to kafka i.e(Bootstrap-service, server address)
+    .option("kafka.bootstrap.servers", "kafka:9092") #tells spark how to connect to kafka i.e(Bootstrap-service, server address)
     .option("subscribe", "test.public.kraken_ohlc") #tells spark which topic to consume (can be multiple)
     .option("startingOffsets", "earliest") #tells spark to read from begining of the topic
     .load() #tells spark to create a streaming dataframe with all the options provided
@@ -17,7 +17,7 @@ ohlc_stream_df = (
 #At this point, nothing is spark only builds the executing plan and reading begins only when a streaming query is executed
 
 #To see the data, convert the kafka messages from binary to string
-ohlc_df = ohlc_stream_df.select(col("value").cast("string").alias("message"))
+#ohlc_df = ohlc_stream_df.select(col("value").cast("string").alias("message"))
 
 '''
 An example of the json being received from Kafka
@@ -27,7 +27,8 @@ They are having two levels of JSON (Outer Debezium JSON and Inner JSON stored as
 We will need to parse the JSON twice
 Below is the schema for the json
 '''
-raw_ohlc_json_schema = StructType([
+#'after' keyword schema
+after_json_schema = StructType([
     StructField('id', IntegerType(), True),
     StructField('timestamp', StringType(), True),
     StructField('data', StringType(), True)
@@ -35,20 +36,50 @@ raw_ohlc_json_schema = StructType([
 
 debezium_json_schema = StructType([
     StructField('before', StringType(), True),
-    StructType('after', StringType(), True),
+    StructField('after', after_json_schema, True),
     StructField('op', StringType(), True)
 ])
 
 #Parse the kafka JSON inside the received events from Kafka
-parsed_ohlc_df = (
-    ohlc_df
+debezium_df = (
+    ohlc_stream_df
     .selectExpr("CAST(value AS STRING) as json")
     .select(from_json(col('json'), debezium_json_schema).alias('record'))
     .select('record.after.*')
 )
 
+#Parse the inner Kraken JSON schema
+ #define a schema for the inner json
+inner_json_schema = StructType([
+    StructField('error', ArrayType(StringType()), True),
+    StructField('result', MapType(StringType(), ArrayType(ArrayType(StringType()))), True)
+])
+
+ #parse the inner json (data column)
+inner_json_df =  debezium_df.withColumn('parsed', from_json(col('data'), inner_json_schema))
+ 
+#remove the "last" key from the result map. it is not an ohlc pair
+pairs_df =  inner_json_df.withColumn('pairs', map_filter(col('parsed.result'), lambda k, v: k != 'last') )
+
+#convert map into rows
+new_ohlc_df = pairs_df.select(
+    explode('pairs').alias('symbol', 'ohlc')
+)
+
+#Extract the first OHLC record
+final_ohlc_df = new_ohlc_df.select(
+    col('symbol'),
+    col('ohlc')[0][0].alias('time'),
+    col('ohlc')[0][1].alias('open'),
+    col('ohlc')[0][2].alias('high'),
+    col('ohlc')[0][3].alias('low'),
+    col('ohlc')[0][4].alias('close'),
+    col('ohlc')[0][5].alias('vwap'),
+    col('ohlc')[0][6].alias('volume'),
+    col('ohlc')[0][7].alias('trades')
+)
 
 #Use a streaming query
-query = (ohlc_df.writeStream.format("console").outputMode("append").option("truncate", False).start())
+query = (final_ohlc_df.writeStream.format("console").outputMode("append").option("truncate", False).start())
 
 query.awaitTermination()
