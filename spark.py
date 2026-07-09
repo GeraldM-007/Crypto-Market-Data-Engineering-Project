aea1e13cc609f931c43ceda8ea55c569de8a4f7f
GeraldM-007 <geraldmuchuku237@gmail.com>
2026-07-09 12:52:04 +0300

    rewrting transformation

--- spark.py
+++ spark.py
@@ -2,6 +2,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import *
 from pyspark.sql.functions import *
 
+#create a spark application named crypto and load the kafka-connector
 spark = SparkSession.builder.appName("crypto").config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.13:4.0.0").getOrCreate()
 
 #readstream tell spark to use structured streaming instead of onetime batch read
@@ -27,59 +28,96 @@ They are having two levels of JSON (Outer Debezium JSON and Inner JSON stored as
 We will need to parse the JSON twice
 Below is the schema for the json
 '''
-#'after' keyword schema
-after_json_schema = StructType([
-    StructField('id', IntegerType(), True),
-    StructField('timestamp', StringType(), True),
-    StructField('data', StringType(), True)
+#'after' keyword in events schema
+after_schema = StructType([
+    StructField("id", IntegerType()),
+    StructField("timestamp", StringType()),
+    StructField("data", StringType())
 ])
 
-debezium_json_schema = StructType([
-    StructField('before', StringType(), True),
-    StructField('after', after_json_schema, True),
-    StructField('op', StringType(), True)
+#the outer debezium event
+debezium_schema = StructType([
+    StructField("before", StringType()),
+    StructField("after", after_schema),
+    StructField("op", StringType())
 ])
 
-#Parse the kafka JSON inside the received events from Kafka
+#parse the debezium json
 debezium_df = (
     ohlc_stream_df
-    .selectExpr("CAST(value AS STRING) as json")
-    .select(from_json(col('json'), debezium_json_schema).alias('record'))
-    .select('record.after.*')
+    #kafka stores messages as bytes but spark cannot parse bytes as JSON. Convert the binary to String
+    .selectExpr("CAST(value AS STRING) AS json")
+    #convert the created JSON string into a spark struct
+    .select(from_json(col("json"), debezium_schema).alias("record"))
+    #ignore delete events because they have 'after:null'
+    .filter(col("record.after").isNotNull())
+    #expands id, timestamp and data into individual columns
+    .select("record.after.*")
 )
 
-#Parse the inner Kraken JSON schema
- #define a schema for the inner json
-inner_json_schema = StructType([
-    StructField('error', ArrayType(StringType()), True),
-    StructField('result', MapType(StringType(), ArrayType(ArrayType(StringType()))), True)
-])
-
- #parse the inner json (data column)
-inner_json_df =  debezium_df.withColumn('parsed', from_json(col('data'), inner_json_schema))
- 
-#remove the "last" key from the result map. it is not an ohlc pair
-pairs_df =  inner_json_df.withColumn('pairs', map_filter(col('parsed.result'), lambda k, v: k != 'last') )
-
-#convert map into rows
-new_ohlc_df = pairs_df.select(
-    explode('pairs').alias('symbol', 'ohlc')
+# Extract the trading pair name using a regular expression to find the pair name
+''' Example output:
+"result": {"last": 1782518400, "0GUSD": [[1782518400, "0.217", "0.217", "0.212", "0.215", "0.214", "14475.54376", 59], 0.217", "0.212", "0.215", "0.214", "14475.54376", 59]]}
+'''
+pair_df = debezium_df.withColumn(
+    "symbol",
+    regexp_extract(
+        col("data"),
+        r'"result"\s*:\s*\{"last"\s*:\s*[^,]+,\s*"([^"]+)"',
+        1
+    )
 )
 
-#Extract the first OHLC record
-final_ohlc_df = new_ohlc_df.select(
-    col('symbol'),
-    col('ohlc')[0][0].alias('time'),
-    col('ohlc')[0][1].alias('open'),
-    col('ohlc')[0][2].alias('high'),
-    col('ohlc')[0][3].alias('low'),
-    col('ohlc')[0][4].alias('close'),
-    col('ohlc')[0][5].alias('vwap'),
-    col('ohlc')[0][6].alias('volume'),
-    col('ohlc')[0][7].alias('trades')
+# spark builds $.result.symbol and extracts only the OHLC array for that symbol 
+pair_df = pair_df.withColumn(
+    "ohlc_json",
+    expr("get_json_object(data, concat('$.result.', symbol))")
 )
 
-#Use a streaming query
-query = (final_ohlc_df.writeStream.format("console").outputMode("append").option("truncate", False).start())
+# Parse the OHLC JSON. The JSON contains Array, Array and then Values
+''' Example: [[1782518400, "0.217", "0.217", "0.212", "0.215", "0.214", "14475.54376", 59], 0.217", "0.212", "0.215", "0.214", "14475.54376", 59]] '''
+
+ohlc_schema = ArrayType(ArrayType(StringType()))
+
+#convert the json string into a spark array
+pair_df = pair_df.withColumn(
+    "ohlc",
+    from_json(col("ohlc_json"), ohlc_schema)
+)
+
+# Flatten first candle
+#For each symbol get the OHLC values from the first event from the array
+
+final_ohlc_df = pair_df.select(
+    col("symbol"),
+
+    col("ohlc")[0][0].cast("long").alias("time"),
+
+    col("ohlc")[0][1].cast("double").alias("open"),
+
+    col("ohlc")[0][2].cast("double").alias("high"),
+
+    col("ohlc")[0][3].cast("double").alias("low"),
+
+    col("ohlc")[0][4].cast("double").alias("close"),
+
+    col("ohlc")[0][5].cast("double").alias("vwap"),
+
+    col("ohlc")[0][6].cast("double").alias("volume"),
+
+    col("ohlc")[0][7].cast("int").alias("trades"),
+
+    col("timestamp")
+)
+
+# Write Stream
+
+query = (
+    final_ohlc_df.writeStream
+    .format("console")
+    .outputMode("append")
+    .option("truncate", False)
+    .start()
+)
 
 query.awaitTermination()
